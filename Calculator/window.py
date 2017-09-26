61121a1bc65d064e51301f415fdb0d432d25c442
Jully Nobre <jullynobre1@gmail.com>
2017-09-26 13:02:52 -0300

    window

---
+++ Calculator/window.py
@@ -0,0 +1,168 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'window.ui'
+#
+# Created by: PyQt5 UI code generator 5.9
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(328, 378)
+        Form.setStyleSheet("background-color:black;")
+        self.btn1 = QtWidgets.QToolButton(Form)
+        self.btn1.setGeometry(QtCore.QRect(10, 240, 71, 61))
+        self.btn1.setStyleSheet("background-color:rgb(90, 90, 90);\n"
+"font-size:40px;\n"
+"border:0;\n"
+"color: white;")
+        self.btn1.setObjectName("btn1")
+        self.btn7 = QtWidgets.QToolButton(Form)
+        self.btn7.setGeometry(QtCore.QRect(10, 100, 71, 61))
+        self.btn7.setStyleSheet("background-color:rgb(90, 90, 90);\n"
+"font-size:40px;\n"
+"border:0;\n"
+"color: white;")
+        self.btn7.setObjectName("btn7")
+        self.btn4 = QtWidgets.QToolButton(Form)
+        self.btn4.setGeometry(QtCore.QRect(10, 170, 71, 61))
+        self.btn4.setStyleSheet("background-color:rgb(90, 90, 90);\n"
+"font-size:40px;\n"
+"border:0;\n"
+"color: white;")
+        self.btn4.setObjectName("btn4")
+        self.btn6 = QtWidgets.QToolButton(Form)
+        self.btn6.setGeometry(QtCore.QRect(170, 170, 71, 61))
+        self.btn6.setStyleSheet("background-color:rgb(90, 90, 90);\n"
+"font-size:40px;\n"
+"border:0;\n"
+"color: white;")
+        self.btn6.setObjectName("btn6")
+        self.btn8 = QtWidgets.QToolButton(Form)
+        self.btn8.setGeometry(QtCore.QRect(90, 100, 71, 61))
+        self.btn8.setStyleSheet("background-color:rgb(90, 90, 90);\n"
+"font-size:40px;\n"
+"border:0;\n"
+"color: white;")
+        self.btn8.setObjectName("btn8")
+        self.btn5 = QtWidgets.QToolButton(Form)
+        self.btn5.setGeometry(QtCore.QRect(90, 170, 71, 61))
+        self.btn5.setStyleSheet("background-color:rgb(90, 90, 90);\n"
+"font-size:40px;\n"
+"border:0;\n"
+"color: white;")
+        self.btn5.setObjectName("btn5")
+        self.btn9 = QtWidgets.QToolButton(Form)
+        self.btn9.setGeometry(QtCore.QRect(170, 100, 71, 61))
+        self.btn9.setStyleSheet("background-color:rgb(90, 90, 90);\n"
+"font-size:40px;\n"
+"border:0;\n"
+"color: white;")
+        self.btn9.setObjectName("btn9")
+        self.btn2 = QtWidgets.QToolButton(Form)
+        self.btn2.setGeometry(QtCore.QRect(90, 240, 71, 61))
+        self.btn2.setStyleSheet("background-color:rgb(90, 90, 90);\n"
+"font-size:40px;\n"
+"border:0;\n"
+"color: white;")
+        self.btn2.setObjectName("btn2")
+        self.btn3 = QtWidgets.QToolButton(Form)
+        self.btn3.setGeometry(QtCore.QRect(170, 240, 71, 61))
+        self.btn3.setStyleSheet("background-color:rgb(90, 90, 90);\n"
+"font-size:40px;\n"
+"border:0;\n"
+"color: white;")
+        self.btn3.setObjectName("btn3")
+        self.btn0 = QtWidgets.QToolButton(Form)
+        self.btn0.setGeometry(QtCore.QRect(90, 310, 71, 61))
+        self.btn0.setStyleSheet("background-color:rgb(90, 90, 90);\n"
+"font-size:40px;\n"
+"border:0;\n"
+"color: white;")
+        self.btn0.setObjectName("btn0")
+        self.btndiv = QtWidgets.QToolButton(Form)
+        self.btndiv.setGeometry(QtCore.QRect(250, 100, 71, 61))
+        self.btndiv.setStyleSheet("background-color:rgb(0, 0, 108);\n"
+"font-size:40px;\n"
+"border:0;\n"
+"color: white;")
+        self.btndiv.setObjectName("btndiv")
+        self.btnmult = QtWidgets.QToolButton(Form)
+        self.btnmult.setGeometry(QtCore.QRect(250, 170, 71, 61))
+        self.btnmult.setStyleSheet("background-color:rgb(0, 0, 108);\n"
+"font-size:40px;\n"
+"border:0;\n"
+"color: white;")
+        self.btnmult.setObjectName("btnmult")
+        self.btnsun = QtWidgets.QToolButton(Form)
+        self.btnsun.setGeometry(QtCore.QRect(250, 240, 71, 61))
+        self.btnsun.setStyleSheet("background-color:rgb(0, 0, 108);\n"
+"font-size:40px;\n"
+"border:0;\n"
+"color: white;")
+        self.btnsun.setObjectName("btnsun")
+        self.btnsub = QtWidgets.QToolButton(Form)
+        self.btnsub.setGeometry(QtCore.QRect(250, 310, 71, 61))
+        self.btnsub.setStyleSheet("background-color:rgb(0, 0, 108);\n"
+"font-size:40px;\n"
+"border:0;\n"
+"color: white;")
+        self.btnsub.setObjectName("btnsub")
+        self.btnclr = QtWidgets.QToolButton(Form)
+        self.btnclr.setGeometry(QtCore.QRect(10, 310, 71, 61))
+        self.btnclr.setStyleSheet("background-color:rgb(0, 0, 108);\n"
+"font-size:35px;\n"
+"border:0;\n"
+"color: white;")
+        self.btnclr.setObjectName("btnclr")
+        self.btnoff = QtWidgets.QToolButton(Form)
+        self.btnoff.setGeometry(QtCore.QRect(170, 310, 71, 61))
+        self.btnoff.setStyleSheet("background-color:rgb(0, 0, 108);\n"
+"font-size:35px;\n"
+"border:0;\n"
+"color: white;")
+        self.btnoff.setObjectName("btnoff")
+        self.textEdit = QtWidgets.QTextEdit(Form)
+        self.textEdit.setGeometry(QtCore.QRect(3, 20, 321, 61))
+        self.textEdit.setStyleSheet("color:white;\n"
+"text-align:left;\n"
+"font-size:37px;\n"
+"padding-left:5px;")
+        self.textEdit.setObjectName("textEdit")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.btn1.setText(_translate("Form", "1"))
+        self.btn7.setText(_translate("Form", "7"))
+        self.btn4.setText(_translate("Form", "4"))
+        self.btn6.setText(_translate("Form", "6"))
+        self.btn8.setText(_translate("Form", "8"))
+        self.btn5.setText(_translate("Form", "5"))
+        self.btn9.setText(_translate("Form", "9"))
+        self.btn2.setText(_translate("Form", "2"))
+        self.btn3.setText(_translate("Form", "3"))
+        self.btn0.setText(_translate("Form", "0"))
+        self.btndiv.setText(_translate("Form", "/"))
+        self.btnmult.setText(_translate("Form", "X"))
+        self.btnsun.setText(_translate("Form", "+"))
+        self.btnsub.setText(_translate("Form", "-"))
+        self.btnclr.setText(_translate("Form", "CLR"))
+        self.btnoff.setText(_translate("Form", "OFF"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
+    sys.exit(app.exec_())
+
