f2b685f70c521123390b419695c2cf96865e153a
Jully Nobre <jullynobre1@gmail.com>
2017-10-17 14:23:56 -0300

    Changes in calculator

--- Calculator/main.py
+++ Calculator/main.py
@@ -1,11 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'window.ui'
-#
-# Created by: PyQt5 UI code generator 5.9
-#
-# WARNING! All changes made in this file will be lost!
-
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
@@ -39,9 +31,6 @@ class Calculator:
         self.operator = ""
 
     def calc(self):
-        print("op: ", self.operator)
-        print("n1: ", self.n1)
-        print("n2: ", self.n2)
         if self.operator == "+":
             self.sol = float(self.n1) + float(self.n2)
         elif self.operator == "-":
