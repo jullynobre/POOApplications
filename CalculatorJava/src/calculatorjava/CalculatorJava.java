ccd1fe76fe03520b2820a982294c6664940af696
Jully Nobre <jullynobre1@gmail.com>
2017-11-15 18:56:56 -0300

    Form and main in only one file

--- CalculatorJava/src/calculatorjava/CalculatorJava.java
+++
@@ -1,8 +0,0 @@
-package calculatorjava;
-public class CalculatorJava {
-    public static void main(String[] args) {
-        Interface view = new Interface();
-        view.setVisible(true);
-        view.setResizable(false );
-    }
-}
